f90ecca1d03cc87dc2c4bf28238fed6adc2beb64
Gerson Penha <Gerson Penha@DESKTOP-V4ORQ23>
2022-02-16 17:54:31 -0300

    Inclusão da versão com get

--- calculadorarestspring/src/main/java/com/fatec/calculadorarestspring/CalculadorarestspringApplication.java
+++ calculadorarestspring/src/main/java/com/fatec/calculadorarestspring/CalculadorarestspringApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculadorarestspringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CalculadorarestspringApplication.class, args);
 	}
-
 }
